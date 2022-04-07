ea6ef456191b513f70663df14acc6458c89ccc76
ArsenyMaliykov <arsenirag@gmail.com>
2022-04-07 17:53:52 +0300

    initial commit
    *add print

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 print('Hello World')
 print(123) 
-print('Hello Python')
-a=11
+print('WhoCame?')
+print('Привет! Я Арсений')
+print('и что?')
+text = 'Helloy Helloy Helloy'
+text =text . replace('y',' ')
+print(text)
+
+first_name = "Whocame--Mountaindew--Smokazacho"
+words = first_name.split('--')
+print(words)
+new_text = '\n'.join(words)
+print(new_text)
+print(first_name.istitle())
+a=15
 b=15
 c=(a+b)
 print(c)
-print('Привет! Я Арсений')
-print('и что?')
-print('Who came?')
+from time import sleep
+from os import system
+text='МаКс лОх'
+for i in range(2):
+    print(text)
+    text = text.swapcase()
+    sleep(0.5)
+what=input("Что делаем?(+,-,*,):")
+a=float(input("Введи первое число: "))
+b=float(input("Введи второе число: "))
+if what=="+":
+    c= a+b
+    print("Результат: "+str(c))
+elif what=="-":
+    c= a-b
+    print("Результат: "+str(c))
+elif what=="*":
+    c=a*b
+    print("Результат: "+str(c))
