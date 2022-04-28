060be601038e253e4018159d9cac2f50e0f55829
ArsenyMaliykov <arsenirag@gmail.com>
2022-04-28 14:18:20 +0300

    initial commit
    *add print

--- main.py
+++ main.py
@@ -32,8 +32,50 @@
 #while number.isdigit():
 #number = int(number)
 
-numbers = '4 5 6 7 8 9 1 2'
-numbers = numbers.split(' ')
-for i,number in enumerate(numbers):
-    numbers[i] = int(number)
+#numbers = '4 5 6 7 8 9 1 2'
+#numbers = numbers.split(' ')
+#for i,number in enumerate(numbers):
+    #numbers[i] = int(number)
+#print(numbers)
+
+#N = int(input('Количество: '))
+#M = int(input('Кратные: '))
+#K = int(input('Нижний предел: '))
+##for i in range(K, N*M+K+1):
+    #if not i % M:
+        #print(i)
+
+#вводится текст,подсичтать количество гласный и согласный в самом тексте
+#def foo(a,b,  *args, **kwargs):
+    #print(a)
+   #print(b)
+    #print(args)
+    #print(kwargs)
+#foo(1,2,3,4,5,6,7)
+
+#lst = [1,2,3,4,5,5,7,8]
+#for i in range(len(lst)+1):
+    #if lst[i] > lst[i+1]:
+        #print(i+1)
+        #break
+    #else:
+        #print(True)
+
+import itertools
+
+
+# 0 2 4 6 8
+#for i in itertools.count(0,2):
+    #if i >= 10:
+       #break
+    #print(i)
+
+from random import randint
+
+#пузырьковый метод(много циклов)
+numbers = [randint(1,123)for _ in range(10)]
+for _ in range(len(numbers)):
+    for i in range(len(numbers) - 1):
+        if numbers[i] > numbers[i+1]:
+            numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
 print(numbers)
